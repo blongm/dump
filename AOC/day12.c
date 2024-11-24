4892c18b452e7653c28db824bb19d914c8603d23
Peter <peterbalta12@gmail.com>
2024-11-24 23:19:40 +0000

    cont.

--- AOC/day12.c
+++ AOC/day12.c
@@ -2,16 +2,29 @@
 #include <stdio.h>
 
 // Load file
-void file_open(char* file_name)
+FILE* open(char* file_name)
 {
-    return file_open(file_name, "rb");
+    return fopen(file_name, "rb");
 }
 
+char* getl(file)
+{
+    char* line = malloc(0);
+    size_t line_len = 0;
+    char c;
+
+    while (c = fgetc(file) != '\n')
+    {
+        line = realloc()
+    }
+}
 
 int main(int argc, char** argv)
 {
     if (argc < 1) return 1;
-    FILE* file = file_open(argv[1]);
+    FILE* file = open(argv[1]);
+
+
 
     return 0;
 }
